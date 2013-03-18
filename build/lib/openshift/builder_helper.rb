07fc665ea1225759dbbf9740272eda19744ebcf5
Dan McPherson <dmcphers@redhat.com>
2013-03-18 14:14:28 -0400

    add cart to cart repo as part of sync

--- build/lib/openshift/builder_helper.rb
+++ build/lib/openshift/builder_helper.rb
@@ -82,6 +82,16 @@ module OpenShift
             exit 1
           end
         end
+        if build_dir =~ /\/cartridges\/openshift-origin-cartridge-(.*)/
+          short_cart_name = $1
+          cart_install_dir = "/usr/libexec/openshift/cartridges/v2/#{short_cart_name}"
+          if File.exists? cart_install_dir
+            unless run("oo-admin-cartridge --action install --source #{cart_install_dir}", :verbose => options.verbose?)
+              FileUtils.rm_rf '/tmp/devenv/sync/'
+              exit 1
+            end
+          end
+        end
       end
     end
 
